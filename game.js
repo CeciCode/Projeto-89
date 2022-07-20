1a59d780bf2c502ba6946611e5a8ca183aa36d84
CeciCode <101422795+CeciCode@users.noreply.github.com>
2022-07-20 11:51:23 -0300

    Add files via upload

--- game.js
+++ game.js
@@ -10,3 +10,26 @@ function send() {
     document.getElementById("numero1").value= "";
     document.getElementById("numero2").value= "";
 }
+qTurn= "player1";
+aTurn= "player2";
+function check() {
+    get_answer= document.getElementById("input_check_box").value;
+    if(get_answer == actual_answer) {
+        if(aTurn == "player1") {
+            update_player1_score= playerScore1 + 1;
+            document.getElementById("playerScore1").innerHTML= update_player1_score;
+        }
+        else {
+            update_player2_score= playerScore2 + 1;
+            document.getElementById("playerScore2").innerHTML= update_player2_score;
+        }
+        if(qTurn == "player1") {
+            qTurn= "player2";
+            document.getElementById("qTurn").innerHTML= "Turno de pergunta: " + playerName2;
+        }
+        else {
+            qTurn= "player1"; 
+            document.getElementById("qTurn").innerHTML= "Turno de pergunta: " + playerName1;
+        }
+    }
+}
